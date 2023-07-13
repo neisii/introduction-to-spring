eb15ec456ee43e222dc1d18837c8d5ce4fd0554d
NEISII <neisii@outlook.com>
2023-07-13 22:23:33 +0900

    Section 5 회원 관리 예제 - 웹 MVC 개발(완)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,21 +1,43 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
-// @Autowired private final MemberService memberService; // 필드 주입: 별로 안 좋다. 중간에 바꿔치기 불가
-  private final MemberService memberService;
 
-//  @Autowired // Setter 주입 Setter Injection: public이라 바꿔치기 당할 수 있다.
-//  public setMemberService(MemberService memberService) {
-//    this.memberService = memberService;
-//  }
+  private final MemberService memberService;
 
   @Autowired // 생성자 주입: 권장 사항1
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
   }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+  @PostMapping("/members/new")
+  public String create(MemberForm memberForm) {
+    Member member = new Member();
+    member.setName(memberForm.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
+  }
+
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
+  }
+
 }
